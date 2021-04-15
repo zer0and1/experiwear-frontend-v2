ee060dda40f11dddd60149526682f5da5c924185
Ilya Moroz <ilya.moroz@voxsync.com>
2021-04-15 13:42:24 +0300

    multiple samples from one user representation

--- containers/Accelerometer/AccChart/index.js
+++ containers/Accelerometer/AccChart/index.js
@@ -18,33 +18,38 @@ const hackNumbers = (value) => value < 10 ? value : (value * 10 / 65526) // toDo
 
 const AccChart = ({ selectedItem }) => {
   const classes = useStyles()
-
   const [page, setPage] = useState(1)
 
   const { accelerometerData: { results, total } } = useSelector(state => state.accelerometer)
 
-  const accFrame = results[0]?.frames[page - 1]
+  const result = results[page - 1]
+
+  if (!result && page !== 1) {
+    setPage(1)
+  }
+
+  const nFrames = result?.frames.length
+  const accData = result?.frames?.map(i => i.data).flat()
 
   let data = {}
-  if (accFrame) {
-    const accFrameData = accFrame.data
+  if (accData) {
     data = {
-      labels: range(0, 33 - 1, 50),
+      labels: range(0, nFrames * 33 - 1, 50),
       datasets: [{
-        label: 'x axis',
-        data: accFrameData.map(i => hackNumbers(i[0])),
+        label: 'x',
+        data: accData.map(i => hackNumbers(i[0])),
         fill: false,
         borderColor: 'rgb(188,19,19)',
         tension: 0.1
       }, {
-        label: 'y axis',
-        data: accFrameData.map(i => hackNumbers(i[1])),
+        label: 'y',
+        data: accData.map(i => hackNumbers(i[1])),
         fill: false,
         borderColor: 'rgb(87,192,75)',
         tension: 0.1
       }, {
-        label: 'z axis',
-        data: accFrameData.map(i => hackNumbers(i[2])),
+        label: 'z',
+        data: accData.map(i => hackNumbers(i[2])),
         fill: false,
         borderColor: 'rgb(9,88,224)',
         tension: 0.1
@@ -67,7 +72,7 @@ const AccChart = ({ selectedItem }) => {
             />
             <Line data={data}/>
             <div className={classes.paginationContainer}>
-              <Pagination count={total} onChange={handleChange} variant="outlined" color="secondary"/>
+              <Pagination count={total} page={page} onChange={handleChange} variant="outlined" color="secondary"/>
             </div>
           </> :
           <>
