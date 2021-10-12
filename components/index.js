e6ce9242acf5b052ef3a5d7528dd589b85c694a4
devofficer <sompaqueen47@gmail.com>
2021-10-12 22:06:59 +0900

    [fix] components exports indexing

--- components/index.js
+++ components/index.js
@@ -3,4 +3,6 @@ export { default as Calendar } from './Calendar';
 export { default as Timeline } from './Timeline';
 export { default as AlertItem } from './AlertItem';
 export { default as AlertField } from './AlertField';
+export { default as BandLogo } from './BandLogo';
+export { default as Logo } from './Logo';
 export * from './UI';
