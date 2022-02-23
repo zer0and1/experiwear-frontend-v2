f9fa64693b7db1ee95d5990ccb34d6d06a8ccb64
devofficer <sompaqueen47@gmail.com>
2022-02-24 05:45:31 +0900

    [fix] api endpoint

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,11 @@
 export const IS_PRODUCT = process.env.CONFIG_ENV === 'production';
 export const PROXY_URL = IS_PRODUCT
   ? 'https://hawks.api.experiwear.com/'
-  : 'https://hawks-dev.api.experiwear.com/api/';
+  : 'https://hawks-dev.experiwear.com/api/';
 
 export const SOCKET_URL = IS_PRODUCT
   ? 'https://hawks.api.experiwear.com/users'
-  : 'https://hawks-dev.api.experiwear.com/users';
+  : 'https://hawks-dev.experiwear.com/users';
 
 export const COOKIE_NAME = 'fan_sid';
 export const COOKIE_MAX_AGE = 24 * 60 * 60 * 1000;
