e7433d7fa74627b7650c5df8e2aa8210030aa7d2
devofficer <sompaqueen47@gmail.com>
2022-02-24 04:44:03 +0900

    [fix] socket.io namespace to users

--- src/config/index.js
+++ src/config/index.js
@@ -4,8 +4,8 @@ export const PROXY_URL = IS_PRODUCT
   : 'https://hawks-dev.experiwear.com/api';
 
 export const SOCKET_URL = IS_PRODUCT
-  ? 'https://hawks.api.experiwear.com/'
-  : 'https://hawks-dev.experiwear.com/';
+  ? 'https://hawks.api.experiwear.com/users'
+  : 'https://hawks-dev.experiwear.com/users';
 
 export const COOKIE_NAME = 'fan_sid';
 export const COOKIE_MAX_AGE = 24 * 60 * 60 * 1000;
