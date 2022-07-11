a6aaaf492e6c08f77597643a5478b1481d04482b
devofficer <sompaqueen47@gmail.com>
2022-07-11 15:32:30 +1000

    fix: push mark lable of duration slider to top

--- src/components/styled-components.js
+++ src/components/styled-components.js
@@ -31,52 +31,6 @@ export const SubHeaderText = withStyles((theme) => ({
   },
 }))(Typography);
 
-// export const PrettoSlider = withStyles((theme) => ({
-//   root: {
-//     color: theme.palette.info.main,
-//   },
-//   track: {
-//     height: 18,
-//     borderRadius: 9,
-//   },
-//   rail: {
-//     height: 18,
-//     borderRadius: 9,
-//     backgroundColor: '#9ea3ba',
-//     opacity: 1,
-//   },
-//   mark: {
-//     display: 'none',
-//   },
-//   markLabel: {
-//     display: 'none',
-//   },
-//   valueLabel: {
-//     fontSize: 16,
-//     fontWeight: 'normal',
-//     top: 40,
-//     left: 'unset',
-//     backgroundColor: 'unset',
-//     color: theme.palette.text.primary,
-//     '&:before': {
-//       display: 'none',
-//     },
-//     '& *': {
-//       background: 'transparent',
-//       color: theme.palette.mode === 'dark' ? '#fff' : '#000',
-//     },
-//   },
-//   thumb: {
-//     width: 41,
-//     height: 41,
-//     marginTop: -10,
-//     marginLeft: -10,
-//     border: '10px solid #fff',
-//     color: theme.palette.info.main,
-//     boxShadow: 'none !important',
-//   },
-// }))(Slider);
-
 export const PrettoSlider = withStyles((theme) => ({
   root: {
     color: theme.palette.info.main,
@@ -95,7 +49,7 @@ export const PrettoSlider = withStyles((theme) => ({
   valueLabel: {
     fontFamily: theme.custom.fonts.SFUITextRegular,
     fontSize: 14,
-    top: 26,
+    top: -18,
     left: 'unset',
     backgroundColor: 'unset',
     '&:before': {
